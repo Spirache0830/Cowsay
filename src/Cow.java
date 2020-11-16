67bd17c95069336d3e0caede78d6900e99243eb8
Alex Spirache <spirache0830@gmail.com>
2020-11-15 20:53:12 -0500

    First Commit

---
+++ src/Cow.java
@@ -0,0 +1,32 @@
+public class Cow {
+
+    private String name;
+    private String image;
+
+    public Cow (String name) {
+
+        this.name = name;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public String getImage() {
+
+        return image;
+
+    }
+
+    public void setImage(String _image) {
+
+        this.image = image;
+
+    }
+
+
+
+}
---
+++ src/Cowsay.java
@@ -0,0 +1,21 @@
+public class Cowsay {
+
+    public static void main(String[] args) {
+
+        Cow[] cowArray = HeiferGenerator.getCows();
+
+        switch (args){
+
+            case (args[0].equals("-l")):
+                break;
+            case (args[0]).equals("-n"):
+                break;
+            default:
+                break;
+
+
+        }
+
+    }
+
+}
